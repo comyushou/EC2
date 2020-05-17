a5f7d1d4eba6220660a384289d59949bddf026fb
vagrant <vagrant@localhost.localdomain>
2020-05-17 04:32:41 +0000

    [ADD]コントローラー

---
+++ app/controllers/users/orders_controller.rb
@@ -0,0 +1,21 @@
+class Users::OrdersController < ApplicationController
+	def new
+		
+	end
+	def create
+		
+	end
+	def index
+		
+	end
+	def show
+		
+	end
+end
+
+	def confirm
+		
+	end
+	def complete
+		
+	end
---
+++ app/helpers/users/adresses_helper.rb
@@ -0,0 +1,2 @@
+module Users::AdressesHelper
+end
