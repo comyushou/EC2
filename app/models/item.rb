4c5ac60609a80579304c10f586026708e1877c30
vagrant <vagrant@localhost.localdomain>
2020-05-17 08:57:32 +0000

    devise修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 	belongs_to :genre
 
-	has_many :Cart_items, dependent: :destroy
-	has_many :oreder_items, dependent: :destroy
+	has_many :cart_items, dependent: :destroy
+	has_many :order_items, dependent: :destroy
 end
