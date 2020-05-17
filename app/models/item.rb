d1d9305095f2c1d1933bb9c589682f221e086085
motokawa624 <61586261+motokawa624@users.noreply.github.com>
2020-05-17 14:03:56 +0900

    Revert "Mizuta"

--- app/models/item.rb
+++
@@ -1,6 +0,0 @@
-class Item < ApplicationRecord
-	belongs_to :genre
-
-	has_many :Cart_items, dependent: :destroy
-	has_many :oreder_items, dependent: :destroy
-end
--- app/models/order.rb
+++
@@ -1,5 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :user
-
-  has_many :oreder_items
-end
